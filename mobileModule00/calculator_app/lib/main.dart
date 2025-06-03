eb8cafc9ccca11fe1e56db4726a0e63fcd0d7a2c
Ocyn <niko.ocyn@gmail.com>
2025-06-03 19:43:58 +0200

    Calculator app flutter project created

---
+++ mobileModule00/calculator_app/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Namer App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isToggled = true;
+
+  void toggle() {
+    setState(() {
+      isToggled = !isToggled;
+    });
+    print('Toggled: $isToggled');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: MegaCard(text: isToggled ? 'A simple text' : 'hello world'),
+          ),
+          ElevatedButton(onPressed: toggle, child: Text('Click me')),
+        ],
+      ),
+    );
+  }
+}
+
+class MegaCard extends StatelessWidget {
+  final String text;
+
+  const MegaCard({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(children: [Text(text, style: style)]),
+      ),
+    );
+  }
+}
